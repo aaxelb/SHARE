3da02a21e9a3e9f4c91a16b79b52a47960b893b9
Abram Booth <boothaa@gmail.com>
2022-03-10 15:42:03 -0500

    wipapi

--- api/suids/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import SimpleRouter
-from api.suids import views
-
-
-router = SimpleRouter()
-router.register(r'suids', views.SuidViewSet, basename='suid')
-urlpatterns = router.urls
