51cb6145b2feaabc12b5b7c1bad06add1222c5e4
Abram Booth <boothaa@gmail.com>
2022-11-14 10:50:11 -0500

    wip

---
+++ share/search/index_strategy/trove_v0.py
@@ -0,0 +1,68 @@
+from share.search.index_strategy.elastic8 import Elastic8IndexStrategy
+from share.search.index_strategy.messages import MessageType
+
+
+class TroveV0IndexStrategy(IndexStrategy):
+    @property
+    def supported_message_types(self):
+        return {
+            MessageType.INDEX_SUID,
+            MessageType.BACKFILL_SUID,
+        }
+
+    @property
+    def index_settings(self):
+        return {}
+
+    @property
+    def index_mappings(self):
+        return {
+            'metadata_record': {
+                'dynamic': 'strict',
+                'properties': {
+                    'vocab_irl': {
+                    },
+                    'keyword_irl': {
+                    },
+                    'reference_irl': {
+                    },
+                    'outcome_irl': {
+                    },
+                }
+            }
+        }
+
+    def build_action_generator(self, index_name, message_type):
+        self.assert_message_type(message_type)
+
+        action_template = {
+            '_index': index_name,
+            '_type': 'metadata_record',
+        }
+
+        def action_generator(target_id_iter):
+            for target_id in target_id_iter:
+                try:
+
+                    nd = NormalizedData.objects.get(
+                        suid_id__in=target_id_iter,
+                        record_format='sharev2_elastic',  # TODO specify in config? or don't
+                    )
+            for record in record_qs:
+                source_doc = json.loads(record.formatted_metadata)
+                if source_doc.pop('is_deleted', False):
+                    action = {
+                        **action_template,
+                        '_id': source_doc['id'],
+                        '_op_type': 'delete',
+                    }
+                else:
+                    action = {
+                        **action_template,
+                        '_id': source_doc['id'],
+                        '_op_type': 'index',
+                        '_source': source_doc,
+                    }
+                yield (record.suid_id, action)
+        return action_generator
+
