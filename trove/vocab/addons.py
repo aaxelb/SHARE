ddbf19addfd7b7e86249eea6f437145a2aba795e
Abram Booth <boothaa@gmail.com>
2023-11-14 09:11:23 -0500

    wip

--- trove/vocab/addons.py
+++ trove/vocab/addons.py
@@ -8,27 +8,17 @@ from trove.vocab.namespaces import (
 )
 
 
-ADDONS = rdf.IriNamespace('https://addons.osf.io/vocab/2023/')
+# try describing osf:addons data in rdf
 
-EXAMPLE_DATA = {
-    'http://foo.example/storage/checksum': {
+ADDONS = rdf.IriNamespace('https://addons.osf.example/vocab/2023/')
+
+EXAMPLE_ADDONS_DATA = {
+    # osfstorage provider:
+    'https://addons.osf.example/storage/osfstorage': {
         RDF.type: {ADDONS.StorageProvider},
-        FOAF.name: {rdf.literal('checksum-keyed archive', language='en')},
+        FOAF.name: {rdf.literal('osf:storage', language='en')},
         DCTERMS.description: {rdf.literal('...')},
-        ADDONS.supportsInterface: {
-            ADDONS.read_by_key,
-            ADDONS.write_by_key,
-            ADDONS.static_key,
-            ADDONS.bulk_download,
-            ADDONS.public_storage,
-            ADDONS.longterm_archive,
-        },
-    },
-    'http://foo.example/storage/dropbox': {
-        RDF.type: {ADDONS.StorageProvider},
-        FOAF.name: {rdf.literal('dropbox maybe', language='en')},
-        DCTERMS.description: {rdf.literal('...')},
-        ADDONS.supportsInterface: {
+        ADDONS.supportedInterface: {
             ADDONS.read_by_key,
             ADDONS.write_by_key,
             ADDONS.copy_by_key,
@@ -39,51 +29,73 @@ EXAMPLE_DATA = {
         },
         ADDONS.maxConcurrentDownloads: {rdf.literal(42)},
         ADDONS.maxUploadMB: {rdf.literal(150)},
-        ADDONS.icon: {'http://foo.example/storage/dropbox/icon.svg'},
+        ADDONS.icon: {'https://addons.osf.example/storage/osfstorage/icon.svg'},
         # ...
     },
-    'http://foo.example/authorized/aoeu': {
-        RDF.type: {ADDONS.AuthorizedStorage},
-        DCTERMS.creator: {'https://osf.io/blarg'},
-        DCAT.accessService: {'http://foo.example/addon/checksum'},
-        ADDONS.authorizedInterface: {
+    # hypothetical checksum-keyed archive:
+    'https://addons.osf.example/storage/checksum': {
+        RDF.type: {ADDONS.StorageProvider},
+        FOAF.name: {rdf.literal('checksum-keyed archive', language='en')},
+        DCTERMS.description: {rdf.literal('(seems like a good idea)')},
+        ADDONS.supportedInterface: {
             ADDONS.read_by_key,
             ADDONS.write_by_key,
-            ADDONS.static_key,
-        },
-        # auth stuff...
-    },
-    'http://foo.example/connected/snth': {
-        RDF.type: {ADDONS.ConnectedStorage},
-        ADDONS.authorizedStorage: {'http://foo.example/authorized/aoeu'},
-        ADDONS.connectedResource: {'https://osf.io/prjct'},
-        ADDONS.connectedInterface: {
-            ADDONS.read_by_key,
+            ADDONS.static_key,  # guaranteed not to change (until checksum collision)
+            ADDONS.bulk_download,
+            ADDONS.public_storage,
+            ADDONS.longterm_archive,
         },
     },
-    'http://foo.example/authorized/my-dropbox': {
+    # authorization for osf:storage:
+    'https://addons.osf.example/authorized/my-osfstorage': {
         RDF.type: {ADDONS.AuthorizedStorage},
         DCTERMS.creator: {'https://osf.io/blarg'},
-        DCAT.accessService: {'http://foo.example/addon/dropbox'},
+        DCAT.accessService: {'https://addons.osf.example/addon/osfstorage'},
         ADDONS.authorizedInterface: {
             ADDONS.read_by_key,
             ADDONS.write_by_key,
             ADDONS.copy_by_key,
-            ADDONS.path_key,
+            ADDONS.path_key,  # supports filetree-like operations on key
             ADDONS.versioned_key,
             ADDONS.bulk_download,
             ADDONS.public_storage,
         },
-        # auth stuff...
+        ADDONS.credentials: {
+            # credential object(s)... TODO: find a standard vocab for that
+        },
     },
-    'http://foo.example/connected/my-dropbox': {
+    # authorization for checksum-archive:
+    'https://addons.osf.example/authorized/aoeu': {
+        RDF.type: {ADDONS.AuthorizedStorage},
+        DCTERMS.creator: {'https://osf.io/blarg'},
+        DCAT.accessService: {'https://addons.osf.example/addon/checksum'},
+        ADDONS.authorizedInterface: {  # can auth less than is supported (but not more)
+            ADDONS.read_by_key,
+            ADDONS.write_by_key,
+            ADDONS.static_key,
+        },
+        ADDONS.credentials: {
+            # credential object(s)... TODO: find a standard vocab for that
+        },
+    },
+    # connecting osf:storage to a project:
+    'https://addons.osf.example/connected/my-osfstorage': {
         RDF.type: {ADDONS.ConnectedStorage},
-        ADDONS.authorizedStorage: {'http://foo.example/authorized/my-dropbox'},
+        ADDONS.authorizedStorage: {'https://addons.osf.example/authorized/my-osfstorage'},
         ADDONS.connectedResource: {'https://osf.io/prjct'},
-        ADDONS.connectedInterface: {
+        ADDONS.connectedInterface: {  # can connect less than is auth'd (but not more)
             ADDONS.read_by_key,
             ADDONS.write_by_key,
             ADDONS.path_key,
+        },
+    },
+    # connecting checksum-archive to a project:
+    'https://addons.osf.example/connected/snth': {
+        RDF.type: {ADDONS.ConnectedStorage},
+        ADDONS.authorizedStorage: {'https://addons.osf.example/authorized/aoeu'},
+        ADDONS.connectedResource: {'https://osf.io/prjct'},
+        ADDONS.connectedInterface: {
+            ADDONS.read_by_key,  # read-only connection
         },
     },
 }
@@ -92,7 +104,25 @@ ADDONS_VOCAB = {
     ADDONS.ExternalAccount: {
         RDF.type: {RDFS.Class}
     },
-    ADDONS.supportsInterface: {
+    ADDONS.supportedInterface: {
+        RDF.type: {RDF.Property}
+    },
+    ADDONS.authorizedInterface: {
+        RDF.type: {RDF.Property}
+    },
+    ADDONS.connectedInterface: {
+        RDF.type: {RDF.Property}
+    },
+    ADDONS.StorageProvider: {
+        RDF.type: {RDFS.Class, ADDONS.Category},
+    },
+    ADDONS.authorizedStorage: {
+        RDF.type: {RDF.Property}
+    },
+    ADDONS.connectedResource: {
+        RDF.type: {RDF.Property}
+    },
+    ADDONS.authorizedStorage: {
         RDF.type: {RDF.Property}
     },
     ADDONS.read_by_key: {
@@ -107,7 +137,5 @@ ADDONS_VOCAB = {
     ADDONS.path_key: {
         RDF.type: {ADDONS.Interface}
     },
-    ADDONS.StorageProvider: {
-        RDF.type: {RDFS.Class, ADDONS.Category},
-    },
+    # ...
 }
