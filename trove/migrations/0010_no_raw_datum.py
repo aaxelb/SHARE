d86b1433c0038b7d73439c69bde6e870bc91ea32
abram axel booth <boothaa@gmail.com>
2025-06-09 16:45:18 -0400

    trove-migrations

---
+++ trove/migrations/0010_no_raw_datum.py
@@ -0,0 +1,27 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('trove', '0009_expiration_date_indexes'),
+    ]
+
+    operations = [
+        migrations.RemoveConstraint(
+            model_name='archivedindexcardrdf',
+            name='trove_archivedindexcardrdf_uniq_archived_version',
+        ),
+        migrations.RemoveField(
+            model_name='archivedindexcardrdf',
+            name='from_raw_datum',
+        ),
+        migrations.RemoveField(
+            model_name='latestindexcardrdf',
+            name='from_raw_datum',
+        ),
+        migrations.RemoveField(
+            model_name='supplementaryindexcardrdf',
+            name='from_raw_datum',
+        ),
+    ]
